3e693fea5126e66a85ffb86572aae227e5d3f3fd
Prathamesh Mutkure <pmutkure009@gmail.com>
2025-08-20 00:48:46 +0530

    Build Script Prisma Fix

--- render-deploy.sh
+++ render-deploy.sh
@@ -4,6 +4,9 @@ set -e
 echo "Installing dependencies..."
 pnpm install
 
+echo "Generating Prisma client..."
+npx turbo db:generate
+
 echo "Building API and dependencies..."
 pnpx turbo build --filter=api
 
